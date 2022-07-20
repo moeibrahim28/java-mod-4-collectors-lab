c5c978fc375f5cfe76e2a23816a13be67a000da8
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-07-20 13:35:22 -0400

    multiply squares method complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 class Main {
 
     // modify this method
     public static long multiplySquaresOfNumbers(List<Integer> numbers) {
-        // your code here
-        return 0;
+
+        return numbers.stream().map(num -> num*num).collect(Collectors.reducing(1, Math::multiplyExact));
     }
 
     public static void main(String[] args) {
